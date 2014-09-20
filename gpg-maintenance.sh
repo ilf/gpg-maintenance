3ba37bb4df629b5eebfa680f84ca5d4ecd298024
ilf <ilf@zeromail.org>
2014-09-20 21:26:03 +0200

    sh: double-quote $key, remove unnecessary $ from RANDOM

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -6,7 +6,7 @@ while true
 do
 	for key in $(gpg --list-keys --with-colons --with-fingerprint | grep --after-context "1" "^pub" | grep "^fpr" | cut -d ":" -f "10" | sort --random-sort)
 	do
-		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys $key 2> /dev/null
-		sleep $(( ($RANDOM % 1000) + 300))
+		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$key" 2> /dev/null
+		sleep $(( (RANDOM % 1000) + 300))
 	done
 done &
