32bfed3988de69b06a576fe39fb373b1e0f42e29
ilf <ilf@zeromail.org>
2014-10-24 11:38:25 +0200

    gpg-maintenance.sh: move initial sleep into while-loop

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
-sleep 30	# wait for tor on boot
-
 while true
 do
 	for key in $(gpg --list-keys --with-colons --with-fingerprint | grep --after-context "1" "^pub" | grep "^fpr" | cut -d ":" -f "10" | sort --random-sort)
 	do
-		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$key" 2> /dev/null
 		sleep $(( (RANDOM % 1000) + 300))
+		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$key" 2> /dev/null
 	done
 done &
