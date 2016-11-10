76764071e98379a0249a8219dba09e544581c6c6
ilf <ilf@zeromail.org>
2016-11-10 20:40:53 +0100

    fix semantics: s/key/fingerprint/

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -2,9 +2,9 @@
 
 while true
 do
-	for key in $(gpg --list-keys --with-colons --with-fingerprint | grep --after-context "1" "^pub" | grep "^fpr" | cut -d ":" -f "10" | sort --random-sort)
+	for fingerprint in $(gpg --list-keys --with-colons --with-fingerprint | grep --after-context "1" "^pub" | grep "^fpr" | cut -d ":" -f "10" | sort --random-sort)
 	do
 		sleep $(( (RANDOM % 1000) + 300))
-		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$key" 2> /dev/null
+		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$fingerprint" 2> /dev/null
 	done
 done &
