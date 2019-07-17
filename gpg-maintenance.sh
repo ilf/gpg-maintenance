4dd55b8b38c734e9df543fd69328dcc348a4f42f
ilf <ilf@zeromail.org>
2019-07-17 20:55:33 +0200

    remove --with-fingerprint, it's now default

--- gpg-maintenance.sh
+++ gpg-maintenance.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	for fingerprint in $(gpg --list-keys --with-colons --with-fingerprint | grep --after-context "1" "^pub" | grep "^fpr" | cut -d ":" -f "10" | sort --random-sort)
+	for fingerprint in $(gpg --list-keys --with-colons | grep --after-context "1" "^pub" | grep "^fpr" | cut -d ":" -f "10" | sort --random-sort)
 	do
 		sleep $(( $(tr -cd "[:digit:]" < /dev/urandom | head -c 3) + 300 ))
 		gpg --batch --no-tty --no-auto-check-trustdb --refresh-keys "$fingerprint" 2> /dev/null
